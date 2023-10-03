0b8b27b7d6bb760eebf9f684cbfefb6a327dedbb
Ravi <83961191+ravindranantony@users.noreply.github.com>
2023-10-03 15:40:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,15 +24,40 @@ document.addEventListener('DOMContentLoaded', function() {
   // Add task to the DOM
   function addTaskToDOM(taskText) {
     const taskList = document.getElementById('taskList');
-    const listItem = document.createElement('li');
-    listItem.textContent = taskText;
-    taskList.appendChild(listItem);
+    const tableRow = document.createElement('tr');
+  
+    const taskCell = document.createElement('td');
+    taskCell.textContent = taskText;
+  
+    const actionCell = document.createElement('td');
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.onclick = function() {
+      tableRow.remove();
+      deleteTask(taskText);
+    };
+    
+    actionCell.appendChild(deleteButton);
+    
+    tableRow.appendChild(taskCell);
+    tableRow.appendChild(actionCell);
+    taskList.appendChild(tableRow);
   }
   
   // Save task to local storage
   function saveTask(taskText) {
     let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     tasks.push(taskText);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+  
+  // Delete task from local storage
+  function deleteTask(taskText) {
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const index = tasks.indexOf(taskText);
+    if (index > -1) {
+      tasks.splice(index, 1);
+    }
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }
   
